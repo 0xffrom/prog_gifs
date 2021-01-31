30e455a3fbe43c3468f3810febb8f53f3dd74fae
goga133 <asromanyuk@edu.hse.ru>
2021-01-31 18:30:31 +0300

    del

--- app/src/main/java/com/goga133/fintech2021/business_logic/SwitchesButtons.kt
+++
@@ -1,9 +0,0 @@
-package com.goga133.fintech2021.business_logic
-
-import android.view.View
-
-interface SwitchesButtons {
-    fun onClickLeftButton(v: View)
-    
-    fun onClickRightButton(v: View)
-}
